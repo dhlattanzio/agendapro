e2e5ad446d525034e4b9db81bf5b34fa91a9d0cc
Diego Lattanzio <lattanzio.diego@gmail.com>
2024-07-29 15:45:28 -0300

    Add some swagger annotations

--- src/main/java/com/dhlattanzio/agendapro/product/controller/ProductController.java
+++ src/main/java/com/dhlattanzio/agendapro/product/controller/ProductController.java
@@ -6,8 +6,12 @@ import com.dhlattanzio.agendapro.product.controller.dto.request.CreateProductReq
 import com.dhlattanzio.agendapro.product.controller.dto.request.UpdateProductRequest;
 import com.dhlattanzio.agendapro.product.service.ProductService;
 import com.dhlattanzio.agendapro.shared.dto.PageResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,27 +23,51 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
     private final ProductService productService;
 
+    @Operation(summary = "Get all products")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
     @GetMapping
-    public PageResponse<ProductDto> getAll(Pageable pageable, ProductFilters filters) {
+    public PageResponse<ProductDto> getAll(@ParameterObject Pageable pageable,
+                                           @ParameterObject ProductFilters filters) {
         return productService.getAll(pageable, filters);
     }
 
+    @Operation(summary = "Get one product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Not found")
+    })
     @GetMapping("/{id}")
     public ProductDto getOne(@PathVariable Long id) {
         return productService.getOne(id);
     }
 
+    @Operation(summary = "Create one product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created")
+    })
     @PostMapping
     public ResponseEntity<ProductDto> createOne(@RequestBody @Valid CreateProductRequest request) {
         ProductDto product = productService.createOne(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
 
+    @Operation(summary = "Update one product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Not found")
+    })
     @PutMapping("/{id}")
     public ProductDto updateOne(@PathVariable Long id, @RequestBody @Valid UpdateProductRequest request) {
         return productService.updateOne(id, request);
     }
 
+    @Operation(summary = "Delete one product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Not found")
+    })
     @DeleteMapping("/{id}")
     public ProductDto deleteOne(@PathVariable Long id) {
         return productService.deleteOne(id);
--- src/main/java/com/dhlattanzio/agendapro/statistic/controller/StatisticController.java
+++ src/main/java/com/dhlattanzio/agendapro/statistic/controller/StatisticController.java
@@ -3,6 +3,9 @@ package com.dhlattanzio.agendapro.statistic.controller;
 import com.dhlattanzio.agendapro.shared.dto.ListResponse;
 import com.dhlattanzio.agendapro.statistic.controller.dto.StatisticDto;
 import com.dhlattanzio.agendapro.statistic.service.StatisticService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class StatisticController {
     private final StatisticService statisticService;
 
+    @Operation(summary = "Get all statistics")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
     @GetMapping
     public ListResponse<StatisticDto> getAll() {
         return new ListResponse<>(statisticService.getAll());
